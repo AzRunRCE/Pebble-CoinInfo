9e08e0ed7d309d8a3d4f8da2a64a4b80f12ea90b
AzzRun <qmartine@student.42.fr>
2017-07-25 15:34:07 +0200

    release 1.0

--- src/c/main.c
+++ src/c/main.c
@@ -26,16 +26,14 @@ static void inbox_received_callback(DictionaryIterator *iter, void *context) {
     snprintf(s_eth, sizeof(s_eth), "ETH: %s€", ethCurrency);
   }
   strcpy(s_text,s_btc);
-  strncat(s_text,s_eth,sizeof(s_eth));
+  strcat(s_text,s_eth);
   text_layer_set_text(s_text_layer, s_text);
   text_layer_set_text_alignment(s_text_layer, PBL_IF_RECT_ELSE(GTextAlignmentLeft, 
                                                                 GTextAlignmentCenter));
 }
 static void inbox_dropped_callback(AppMessageResult reason, void *context) {
-  // A message was received, but had to be dropped
   APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped. Reason: %d", (int)reason);
 }
-
 
 void handle_init(void) {
   app_message_open(inbox_size, outbox_size);
@@ -46,7 +44,7 @@ void handle_init(void) {
   Layer *window_layer = window_get_root_layer(my_window);
   GRect bounds = layer_get_bounds(window_layer);
   GFont font = fonts_get_system_font(PBL_IF_RECT_ELSE(FONT_KEY_GOTHIC_28_BOLD, FONT_KEY_GOTHIC_28_BOLD));
-  s_text_layer = text_layer_create(GRect(0, 72-14, bounds.size.w, 72));
+  s_text_layer = text_layer_create(GRect(0, 50, bounds.size.w, 72));
   text_layer_set_font(s_text_layer, font);
   layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
 }

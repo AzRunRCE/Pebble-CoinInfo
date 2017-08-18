900dd3059e81e799e5ba6698870676a91b01956b
AzzRun <qmartine@student.42.fr>
2017-08-18 09:51:31 +0200

    Bitcoin Cash Currency & Kraken.com
    Add Bitcoin Cash Currency
    
    Change source for Krak

--- src/c/main.c
+++ src/c/main.c
@@ -2,34 +2,44 @@
 
 Window *my_window;
 TextLayer *s_text_layer;
-const uint32_t inbox_size = 64;
-const uint32_t outbox_size = 64;
-static char s_text[128];
+const uint32_t inbox_size = 128;
+const uint32_t outbox_size = 0;
+static char s_text[72];
 
 enum {
   BTC,
-  ETH
+  ETH,
+  BCH
 };
 
 static void inbox_received_callback(DictionaryIterator *iter, void *context) {
   Tuple *btcCurrencyTuple = dict_find(iter, BTC);
   Tuple *ethCurrencyTuple = dict_find(iter, ETH);
-  static char s_btc[64];
-  static char s_eth[64];
+  Tuple *bchCurrencyTuple = dict_find(iter, BCH);
+  static char s_btc[24];
+  static char s_eth[24];
+  static char s_bch[24];
   
   if(btcCurrencyTuple) {
     char *btcCurrency = btcCurrencyTuple->value->cstring;
     snprintf(s_btc, sizeof(s_btc), "BTC: %s€\n", btcCurrency);
   }
+  strcpy(s_text,s_btc);
+  
   if(ethCurrencyTuple) {
     char *ethCurrency  = ethCurrencyTuple->value->cstring;
-    snprintf(s_eth, sizeof(s_eth), "ETH: %s€", ethCurrency);
+    snprintf(s_eth, sizeof(s_eth), "ETH: %s€\n", ethCurrency);
   }
-  strcpy(s_text,s_btc);
   strcat(s_text,s_eth);
+  
+  if(bchCurrencyTuple) {
+    char *bchCurrency  = bchCurrencyTuple->value->cstring;
+    snprintf(s_bch, sizeof(s_bch), "BCH: %s€", bchCurrency);
+  }
+  strcat(s_text,s_bch);
+   APP_LOG(APP_LOG_LEVEL_ERROR, s_text,NULL);
   text_layer_set_text(s_text_layer, s_text);
-  text_layer_set_text_alignment(s_text_layer, PBL_IF_RECT_ELSE(GTextAlignmentLeft, 
-                                                                GTextAlignmentCenter));
+  text_layer_set_text_alignment(s_text_layer,GTextAlignmentCenter);
 }
 static void inbox_dropped_callback(AppMessageResult reason, void *context) {
   APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped. Reason: %d", (int)reason);
@@ -44,7 +54,7 @@ void handle_init(void) {
   Layer *window_layer = window_get_root_layer(my_window);
   GRect bounds = layer_get_bounds(window_layer);
   GFont font = fonts_get_system_font(PBL_IF_RECT_ELSE(FONT_KEY_GOTHIC_28_BOLD, FONT_KEY_GOTHIC_28_BOLD));
-  s_text_layer = text_layer_create(GRect(0, 50, bounds.size.w, 72));
+  s_text_layer = text_layer_create(GRect(0, bounds.size.h /4, bounds.size.w, 85));
   text_layer_set_font(s_text_layer, font);
   layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
 }
--- src/pkjs/app.js
+++ src/pkjs/app.js
@@ -1,20 +1,24 @@
 Pebble.addEventListener('ready', function() {
-  var data = {BTC:"0", ETH:"0"};
-  console.log('PebbleKit JS ready!');
-  httpRequest("https://api.cryptonator.com/api/ticker/btc-eur","GET",function(text) {
-    var obj = JSON.parse(text);
-    data.BTC =Number(obj.ticker.price).toFixed(2);
-  });
-  httpRequest("https://api.cryptonator.com/api/ticker/eth-eur","GET",function(text) {
-    var obj = JSON.parse(text);
-    data.ETH =Number(obj.ticker.price).toFixed(2);
-    Pebble.sendAppMessage(data, function() {
-      console.log('Message sent successfully: ' + JSON.stringify(data));
-      }, function(e) {
-      console.log('Message failed: ' + JSON.stringify(e));
+  var data = {BTC:"0", ETH:"0",BCH:"0"};
+  console.log('Starting Javascript: ');
+ 
+  httpRequest("https://api.kraken.com/0/public/Trades?pair=BTCEUR","GET",function(text) {
+    var objBTC = JSON.parse(text);
+    data.BTC =Number(objBTC.result.XXBTZEUR[0][0]).toFixed(2);
+    httpRequest("https://api.kraken.com/0/public/Trades?pair=ETHEUR","GET",function(text) {
+      var objETH = JSON.parse(text);
+      data.ETH = Number(objETH.result.XETHZEUR[0][0]).toFixed(2);
+      httpRequest("https://api.kraken.com/0/public/Trades?pair=BCHEUR","GET",function(text) {
+        var objBCH = JSON.parse(text);
+        data.BCH = Number(objBCH.result.BCHEUR[0][0]).toFixed(2);
+        Pebble.sendAppMessage(data, function() {
+          console.log('Message sent successfully: ' + JSON.stringify(data));
+        }, function(e) {
+          console.log('Message failed: ' + JSON.stringify(e));
+        });
       });
+    });
   });
-
 });
 
 function httpRequest(url,method,callback) {

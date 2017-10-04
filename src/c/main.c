4cb042a9d9ab824236e7fbb3c1580e503d99822c
AzzRun <qmartine@student.42.fr>
2017-10-04 16:48:41 +0200

    New coins
    BCH 
    LTC

--- src/c/main.c
+++ src/c/main.c
@@ -4,40 +4,42 @@ Window *my_window;
 TextLayer *s_text_layer;
 const uint32_t inbox_size = 128;
 const uint32_t outbox_size = 0;
-static char s_text[72];
+static char s_text[94];
 
 enum {
   BTC,
   ETH,
-  BCH
+  BCH,
+  LTC
 };
 
 static void inbox_received_callback(DictionaryIterator *iter, void *context) {
   Tuple *btcCurrencyTuple = dict_find(iter, BTC);
   Tuple *ethCurrencyTuple = dict_find(iter, ETH);
   Tuple *bchCurrencyTuple = dict_find(iter, BCH);
-  static char s_btc[24];
-  static char s_eth[24];
-  static char s_bch[24];
+  Tuple *ltcCurrencyTuple = dict_find(iter, LTC);
+  
+  static char s_tmp[24];
+
   
   if(btcCurrencyTuple) {
-    char *btcCurrency = btcCurrencyTuple->value->cstring;
-    snprintf(s_btc, sizeof(s_btc), "BTC: %s€\n", btcCurrency);
+    snprintf(s_tmp, sizeof(s_tmp), "BTC: %s€\n", btcCurrencyTuple->value->cstring);
+    strcpy(s_text,s_tmp);
   }
-  strcpy(s_text,s_btc);
-  
   if(ethCurrencyTuple) {
-    char *ethCurrency  = ethCurrencyTuple->value->cstring;
-    snprintf(s_eth, sizeof(s_eth), "ETH: %s€\n", ethCurrency);
+    snprintf(s_tmp, sizeof(s_tmp), "ETH: %s€\n",  ethCurrencyTuple->value->cstring);
+    strcat(s_text,s_tmp);
   }
-  strcat(s_text,s_eth);
-  
   if(bchCurrencyTuple) {
-    char *bchCurrency  = bchCurrencyTuple->value->cstring;
-    snprintf(s_bch, sizeof(s_bch), "BCH: %s€", bchCurrency);
+    snprintf(s_tmp, sizeof(s_tmp), "BCH: %s€\n", bchCurrencyTuple->value->cstring);
+    strcat(s_text,s_tmp);
   }
-  strcat(s_text,s_bch);
-   APP_LOG(APP_LOG_LEVEL_ERROR, s_text,NULL);
+  if(bchCurrencyTuple) {
+    snprintf(s_tmp, sizeof(s_tmp), "LTC: %s€", ltcCurrencyTuple->value->cstring);
+    strcat(s_text,s_tmp);
+  }
+ 
+  APP_LOG(APP_LOG_LEVEL_ERROR, s_text,NULL);
   text_layer_set_text(s_text_layer, s_text);
   text_layer_set_text_alignment(s_text_layer,GTextAlignmentCenter);
 }
@@ -54,7 +56,7 @@ void handle_init(void) {
   Layer *window_layer = window_get_root_layer(my_window);
   GRect bounds = layer_get_bounds(window_layer);
   GFont font = fonts_get_system_font(PBL_IF_RECT_ELSE(FONT_KEY_GOTHIC_28_BOLD, FONT_KEY_GOTHIC_28_BOLD));
-  s_text_layer = text_layer_create(GRect(0, bounds.size.h /4, bounds.size.w, 85));
+  s_text_layer = text_layer_create(GRect(0, 20, bounds.size.w,  120));
   text_layer_set_font(s_text_layer, font);
   layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
 }
--- src/pkjs/app.js
+++ src/pkjs/app.js
@@ -1,30 +1,35 @@
 Pebble.addEventListener('ready', function() {
-  var data = {BTC:"0", ETH:"0",BCH:"0"};
+  var data = {BTC:"0", ETH:"0",BCH:"0",LTC:"0"};
   console.log('Starting Javascript: ');
  
-  httpRequest("https://api.kraken.com/0/public/Trades?pair=BTCEUR","GET",function(text) {
-    var objBTC = JSON.parse(text);
-    data.BTC =Number(objBTC.result.XXBTZEUR[0][0]).toFixed(2);
-    httpRequest("https://api.kraken.com/0/public/Trades?pair=ETHEUR","GET",function(text) {
-      var objETH = JSON.parse(text);
-      data.ETH = Number(objETH.result.XETHZEUR[0][0]).toFixed(2);
-      httpRequest("https://api.kraken.com/0/public/Trades?pair=BCHEUR","GET",function(text) {
-        var objBCH = JSON.parse(text);
-        data.BCH = Number(objBCH.result.BCHEUR[0][0]).toFixed(2);
+  httpRequest("https://api.kraken.com/0/public/Trades?pair=BTCEUR","GET",function(result) {
+    data.BTC =Number(result.XXBTZEUR[result.XXBTZEUR.length - 1][0]).toFixed(2); 
+    httpRequest("https://api.kraken.com/0/public/Trades?pair=ETHEUR","GET",function(result) {
+      data.ETH = Number(result.XETHZEUR[result.XETHZEUR.length - 1][0]).toFixed(2);
+      httpRequest("https://api.kraken.com/0/public/Trades?pair=BCHEUR","GET",function(result) {
+        data.BCH = Number(result.BCHEUR[result.BCHEUR.length - 1][0]).toFixed(2);
+        httpRequest("https://api.kraken.com/0/public/Trades?pair=LTCEUR","GET",function(result) {
+          data.LTC = Number(result.XLTCZEUR[result.XLTCZEUR.length - 1][0]).toFixed(2);
+        
         Pebble.sendAppMessage(data, function() {
           console.log('Message sent successfully: ' + JSON.stringify(data));
         }, function(e) {
           console.log('Message failed: ' + JSON.stringify(e));
         });
+        
+        
       });
     });
   });
 });
+});
+    
+
 
 function httpRequest(url,method,callback) {
   var xhr = new XMLHttpRequest();
   xhr.onload =function () {
-   callback(this.responseText);
+   callback(JSON.parse(this.responseText).result);
   };
   xhr.open(method, url);
   xhr.send();
